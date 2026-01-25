c00b0e5463fbcd6b0427b8815a918d9d1550a058
Oskar Skoog <oskar@osd.se>
2026-01-26 00:12:54 +0100

    Bump to dev version

--- markdocpy/version.py
+++ markdocpy/version.py
@@ -4,4 +4,4 @@
 # - Use .devN suffix during development - add after making a release
 # - Remove .devN for stable releases
 # - Increment MINOR for new features, MAJOR for breaking changes
-__version__ = "0.3.0"
+__version__ = "0.3.0.dev1"
